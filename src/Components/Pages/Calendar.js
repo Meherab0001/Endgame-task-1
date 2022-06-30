fa83bfbe3f1b8b98aca6635cca5c85cc27347180
Meherab211 <meherab395@gmail.com>
2022-07-01 00:38:57 +0600

    add calendar

--- src/Components/Pages/Calendar.js
+++ src/Components/Pages/Calendar.js
@@ -1,9 +1,28 @@
 import React from 'react';
-
+import { format } from 'date-fns';
+import { DayPicker } from 'react-day-picker';
+import 'react-day-picker/dist/style.css';
 const Calendar = () => {
+
+    const [selected, setSelected] = React.useState(new Date());
+
+    let footer = <p>Please pick a day.</p>;
+    if (selected) {
+        footer = <p>You picked {format(selected, 'PP')}.</p>;
+    }
     return (
-        <div>
-            <h2>This Calendar route</h2>
+        <div class="hero min-h-screen bg-base-200">
+            <div class="hero-content flex-col lg:flex-row-reverse">
+              
+                <div>
+                    <DayPicker
+                        mode="single"
+                        selected={selected}
+                        onSelect={setSelected}
+                        footer={footer}
+                    />
+                </div>
+            </div>
         </div>
     );
 };
